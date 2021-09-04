ba3762e753d904ede42446d5001681d35560ab20
Brandon Valley <bavvh8@mst.edu>
2021-09-03 19:55:42 -0500

    vid_file_to_transcript_txt works

--- transcript_utils.py
+++ transcript_utils.py
@@ -2,38 +2,68 @@ import speech_recognition as sr
 import moviepy.editor as me
 
 # VIDEO_FILE = "C:/Users/mt204e/Videos/2021-09-03 11-52-22.mkv"
-VIDEO_FILE = "C:\\Users\\Brandon\\Documents\\Other\\temp\\post_0231.mp4"
+# VIDEO_FILE = "C:\\Users\\Brandon\\Documents\\Other\\temp\\post_0231.mp4"
 OUTPUT_AUDIO_FILE = "converted.wav"
-OUTPUT_TEXT_FILE = "recognized.txt"
+# OUTPUT_TEXT_FILE = "recognized.txt"
 # try:
-#     print('starting try...')
-#     video_clip = me.VideoFileClip(r"{}".format(VIDEO_FILE))
-#     video_clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
-#     recognizer =  sr.Recognizer()
-#     audio_clip = sr.AudioFile("{}".format(OUTPUT_AUDIO_FILE))
-#     with audio_clip as source:
-#         audio_file = recognizer.record(source)
-#     print("Please wait ...")
-#     result = recognizer.recognize_google(audio_file)
-#     with open(OUTPUT_TEXT_FILE, 'w') as file:
-#         file.write(result)
-#         print("Speech to text conversion successful.")
-# except Exception as e:
-#     print("Attempt failed -- ", e)
-# try:
-print('starting try...')
-video_clip = me.VideoFileClip(r"{}".format(VIDEO_FILE))
-video_clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
-recognizer =  sr.Recognizer()
-audio_clip = sr.AudioFile("{}".format(OUTPUT_AUDIO_FILE))
-with audio_clip as source:
-    audio_file = recognizer.record(source)
-print("Please wait ...")
-result = recognizer.recognize_google(audio_file)
-with open(OUTPUT_TEXT_FILE, 'w') as file:
-    file.write(result)
+# #     print('starting try...')
+# #     video_clip = me.VideoFileClip(r"{}".format(VIDEO_FILE))
+# #     video_clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
+# #     recognizer =  sr.Recognizer()
+# #     audio_clip = sr.AudioFile("{}".format(OUTPUT_AUDIO_FILE))
+# #     with audio_clip as source:
+# #         audio_file = recognizer.record(source)
+# #     print("Please wait ...")
+# #     result = recognizer.recognize_google(audio_file)
+# #     with open(OUTPUT_TEXT_FILE, 'w') as file:
+# #         file.write(result)
+# #         print("Speech to text conversion successful.")
+# # except Exception as e:
+# #     print("Attempt failed -- ", e)
+# # try:
+# print('starting try...')
+# video_clip = me.VideoFileClip(r"{}".format(VIDEO_FILE))
+# video_clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
+# recognizer =  sr.Recognizer()
+# audio_clip = sr.AudioFile("{}".format(OUTPUT_AUDIO_FILE))
+# with audio_clip as source:
+#     audio_file = recognizer.record(source)
+# print("Please wait ...")
+# result = recognizer.recognize_google(audio_file)
+# with open(OUTPUT_TEXT_FILE, 'w') as file:
+#     file.write(result)
+#     print("Speech to text conversion successful.")
+# # except Exception as e:
+# #     print("Attempt failed -- ", e)
+#     
+# #     pip install SpeechRecognition
+
+def vid_file_to_transcript_txt(vid_file_path, output_txt_file_path):
+    print('starting try...')
+    video_clip = me.VideoFileClip(r"{}".format(vid_file_path))
+    video_clip.audio.write_audiofile(r"{}".format(OUTPUT_AUDIO_FILE))
+    recognizer =  sr.Recognizer()
+    audio_clip = sr.AudioFile("{}".format(OUTPUT_AUDIO_FILE))
+    with audio_clip as source:
+        audio_file = recognizer.record(source)
+    print("Please wait ...")
+    result = recognizer.recognize_google(audio_file)
+    with open(output_txt_file_path, 'w') as file:
+        file.write(result)
     print("Speech to text conversion successful.")
-# except Exception as e:
-#     print("Attempt failed -- ", e)
     
-#     pip install SpeechRecognition
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    vid_file_path = "C:\\Users\\Brandon\\Documents\\Other\\temp\\post_0231.mp4"
+    output_txt_file_path = "recognized.txt"
+    
+    vid_file_to_transcript_txt(vid_file_path, output_txt_file_path)
+    print('done')
+    
+    
+    
+        
